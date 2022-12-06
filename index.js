7017e30749423430662165d1b3c6a25b0df4746f
Kavitha <kavithamd.kavi@gmail.com>
2022-12-07 00:12:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+var readlineSync = require("readline-sync");
+var score = 0;
+var userName = readlineSync.question("May I know your name? ");
+console.log("Welcome " + userName + " to BTS quiz?");
+
+console.log("------------------------");
+
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question);
+  if (userAnswer === answer) {
+    console.log("right👌");
+    score++;
+  } else {
+    console.log("wrong👎");
+  }
+
+  console.log("current score: ", score);
+  console.log("------------");
+}
+
+var questions = [
+  {
+    question: "How many members make the group?",
+    answer: "7",
+  },
+  {
+    question: "Who is the leader of the BTS group? ",
+    answer: "RM",
+  },
+  {
+    question: "Who is the youngest member in the group?",
+    answer: "JK",
+  },
+  {
+    question: "what is the country of BTS? ",
+    answer: "South Korea",
+  },
+  {
+    question: "What are BTS fans called as? ",
+    answer: "ARMY",
+  },
+];
+
+for (var i = 0; i < questions.length; i++) {
+  play(questions[i].question, questions[i].answer);
+}
+
+console.log("Your Total Score is : ", score);
+
+console.log("Thank you for playing");
